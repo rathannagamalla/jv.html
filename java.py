6a93892eca35acf0a084b8da588ac27f6a9d40ba
rathannagamalla <119503302+rathannagamalla@users.noreply.github.com>
2022-12-28 10:03:54 -0800

    Create java.py

---
+++ java.py
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+   <body>
+    <h1>
+          <span>This is a demo for </span>
+          <u><span id="demo"></span></u>
+   </h1>
+   <script>
+       document.getElementById("demo").innerHTML = "script Tag"
+   </script>
+   </body>
+</html>
